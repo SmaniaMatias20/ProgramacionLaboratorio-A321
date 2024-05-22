66c0583743b7283721ecfe47fc1e87de2c4bf003
Matias Smania <matiasezequielsmania@gmail.com>
2024-05-22 10:24:11 -0300

    Nueva prueba subida

--- Smania.Matias.PP/Smania.Matias.PP/Program.cs
+++ Smania.Matias.PP/Smania.Matias.PP/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Principal
+namespace Smania.Matias.PP
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola como estas");
         }
     }
 }
--- Smania.Matias.PP/Smania.Matias.PP/Properties/AssemblyInfo.cs
+++ Smania.Matias.PP/Smania.Matias.PP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Principal")]
+[assembly: AssemblyTitle("Smania.Matias.PP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Principal")]
+[assembly: AssemblyProduct("Smania.Matias.PP")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("37eac775-d648-4df2-ab9f-aa66a78e5c44")]
+[assembly: Guid("1240c718-4803-4e35-99d9-1a98c15c17e5")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
